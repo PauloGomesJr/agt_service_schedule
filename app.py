fc64742efbab1037db3103d6ce2ac369ef14bc81
Paulo Gomes <pago-jr@hotmail.com>
2025-03-19 15:23:13 -0300

    add arquivo json de registro de serviços

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import requests
 
 
-url = 'https://paulogomesjr.github.io/agt_service_schedule/employees.json'
+url = 'https://paulogomesjr.github.io/agt_service_schedule/service_records.json'
 
 response = requests.get(url)
 print(response)
@@ -10,9 +10,22 @@ print(response)
 
 if response.status_code == 200:
     dados_json = response.json()
-    dados_agt = {}
-    
-     
+    dados_service = {}
+    for item in dados_json:
+        service_assig = item['assignments']
+        if service_assig not in dados_service:
+            dados_service[service_assig] = []
+
+        dados_service[service_assig].append({
+            "service_reg": item['service_register'],
+            "place": item['place'],
+            "address": item['address'],
+            "workers": item['workers'],
+            "equipments": item['workers'],
+        })
 else:
     print(f'O erro foi {response.status_code}')
+
+
+print(dados_service['Plumbing'])
 
