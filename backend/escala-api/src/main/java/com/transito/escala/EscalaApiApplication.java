c720cb8fe40efe1e501a82e39961cf9a88fe3caf
Paulo Gomes <pago-jr@hotmail.com>
2025-12-24 17:55:17 -0300

    chore: configuracao da estrutura inicial do projeto para backend e frontend

---
+++ backend/escala-api/src/main/java/com/transito/escala/EscalaApiApplication.java
@@ -0,0 +1,13 @@
+package com.transito.escala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaApiApplication.class, args);
+	}
+
+}
---
+++ backend/escala-api/src/test/java/com/transito/escala/EscalaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transito.escala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
