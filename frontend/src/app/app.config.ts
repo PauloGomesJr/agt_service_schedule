4f2083d006750388a3abc0282d097dbc24ae957d
Paulo Gomes <pago-jr@hotmail.com>
2026-03-04 20:05:42 -0300

    fix(i18n): set default application locale to pt-BR for date formatting

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core'; // Adicione o LOCALE_ID
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor'; // Importe o interceptor
+import { authInterceptor } from './interceptors/auth.interceptor'; // O seu interceptor
+
+// === 1. IMPORTAÇÕES DO IDIOMA PORTUGUÊS ===
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+// === 2. REGISTRA O IDIOMA NO ANGULAR ===
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // Adicionamos o withInterceptors aqui:
-    provideHttpClient(withInterceptors([authInterceptor])) 
+    provideHttpClient(withInterceptors([authInterceptor])),
+    
+    // === 3. AVISA O SISTEMA PARA USAR O PT-BR COMO PADRÃO ===
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
