aa1bbb9d84227dbba33bbec9cd16ed87018a32bd
Paulo Gomes <pago-jr@hotmail.com>
2026-04-03 12:22:00 -0300

    fix: ativa link de registro e melhora feedback de usuario bloqueado

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router,RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth'; // Importando o seu arquivo auth.ts
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+     CommonModule,
+     FormsModule,
+     RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
@@ -44,9 +47,17 @@ export class LoginComponent {
       },
       error: (err) => {
         this.carregando = false;
-        if (err.status === 403 || err.status === 401) {
+        
+        // Verifica se o Java mandou uma mensagem específica em texto (ex: "Sua conta aguarda aprovação...")
+        if (err.status === 403 && typeof err.error === 'string') {
+          this.mensagemErro = err.error;
+        } 
+        // Se for erro normal de senha errada
+        else if (err.status === 403 || err.status === 401) {
           this.mensagemErro = 'Usuário ou senha incorretos.';
-        } else {
+        } 
+        // Se a API estiver fora do ar
+        else {
           this.mensagemErro = 'Erro ao conectar com o servidor.';
         }
       }
